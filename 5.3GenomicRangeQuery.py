b17aa0af6bab0624494c932a0a12d8290cb8b90c
YU RUEI LU <80952286+uray-lu@users.noreply.github.com>
2022-11-20 18:04:17 +0800

    Update and rename GenomicRangeQuery.py to 5.3GenomicRangeQuery.py

---
+++ 5.3GenomicRangeQuery.py
@@ -0,0 +1,13 @@
+def solution(S, P, Q):
+
+    res = []
+    for i in range(len(P)):
+        if 'A' in S[P[i]:Q[i]+1]:
+            res.append(1)
+        elif 'C' in S[P[i]:Q[i]+1]:
+            res.append(2)
+        elif 'G' in S[P[i]:Q[i]+1]:
+            res.append(3)
+        else:
+            res.append(4)
+    return res
