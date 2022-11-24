9f180f20c0faf5123d2909ddbb1cc8117d34ecff
YU RUEI LU <80952286+uray-lu@users.noreply.github.com>
2022-11-24 17:21:07 +0800

    Update NumberOfDiscIntersections.py

--- NumberOfDiscIntersections.py
+++ NumberOfDiscIntersections.py
@@ -1,49 +1,45 @@
 def solution(A):
+    
     if len(A) == 0 or len(A) == 1:
         return 0
-        
-         
-    start = []
-    end = []
-    c_start =[0]*len(A)
-    c_end =[0]*len(A)
-    cc_start =[0]*len(A)
-    cc_end =[0]*len(A)
-    total = 0
-    
-    
+
+    S = [0]*len(A)
+    E = [0]*len(A)
+    CS = [0]*len(A)
+    CE = [0]*len(A)
+
     for i in range(len(A)):
-        if i-A[i]<0:
-            start.append(0)
+        if ((i-A[i])<=0):
+            S[i] = 0
         else:
-            start.append(i-A[i])
-        
-        if i+A[i] > max(A):
-            end.append(max(A))
+            S[i] = i-A[i]
+        if ((i+A[i]) > len(A)-1):
+            E[i] = len(A)-1
         else:
-            end.append(i+A[i])
+            E[i] = i+A[i]
+
+        CS[S[i]] += 1
+        CE[E[i]] += 1
+
+    CCS = [0]*len(A)
+    CCE = [0]*len(A)
+
+    CCS[0] = CS[0]
+    CCE[0] = CE[0]
+    for i in range(1,len(A)):
+        CCS[i] = CS[i] + CCS[i-1]
+        CCE[i] = CE[i] + CCE[i-1]
 
 
-    for element in start:
-        c_start[element] += 1 
-    
-    for element in end:    
-        c_end[element] += 1
-    
-    for k in range(len(c_start)):
-        cc_start[k] = c_start[k]+cc_start[k-1]
-
-    for k in range(len(c_end)):
-        cc_end[k] = c_end[k]+cc_end[k-1]
-
-    
+    tot = 0
     for i in range(len(A)):
         if i == 0:
-            total += (cc_start[i] - c_start[i])*c_start[i] + (c_start[i]*(c_start[i]-1)/2)
+            tot += (CCS[i] - CS[i])*CS[i] + CS[i]*(CS[i]-1)/2
         else:
-            total += (cc_start[i] - cc_end[i-1] - c_start[i])*c_start[i]+ (c_start[i]*(c_start[i]-1)/2)
-
-    if total > 10000000:
+            tot += (CCS[i] - CCE[i-1] - CS[i])*CS[i]+ CS[i]*(CS[i]-1)/2
+        
+    if tot>10000000:
         return -1
+
     else:
-        return int(total) 
+        return int(tot)
