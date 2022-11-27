e89a56abd9c43619cccdf902329c01cb629c6e36
YU RUEI LU <80952286+uray-lu@users.noreply.github.com>
2022-11-27 20:01:39 +0800

    Create 7.4StoneWall.py

---
+++ 7.4StoneWall.py
@@ -0,0 +1,25 @@
+def solution(H):
+    
+
+    topbase = [H[0]]
+    total = 1
+
+    for i in range(len(H)):
+        if H[i] == topbase[-1]:
+            pass
+        else:
+            while len(topbase) != 0:
+                if H[i]  < topbase[-1]:
+                    topbase.pop()
+                    if len(topbase) == 0:
+                        topbase.append(H[i])
+                        total += 1
+                        break
+                elif H[i] > topbase[-1]:
+                    total += 1
+                    topbase.append(H[i])
+                    break
+                elif H[i]  == topbase[-1]:
+                    break           
+
+    return total  
