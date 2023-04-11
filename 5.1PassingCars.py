458d5a85648d64ef4041e5c9d8474c9a57b28494
YU RUEI LU <80952286+uray-lu@users.noreply.github.com>
2023-04-11 23:23:09 +0800

    Update 5.1PassingCars.py

--- 5.1PassingCars.py
+++ 5.1PassingCars.py
@@ -1,22 +1,15 @@
 def solution(A):
     
-    
-    if len(A) == 1:
-        return 0
-    if len(A) == sum(A):
-        return 0
-    if sum(A) == 0:
-        return 0
-    
-    total_pair = 0
     max_pair = sum(A)
-    for element in A:
-        if element == 1:
+    total_pair = 0
+
+    for car in (A):
+        if car == 1:
             max_pair -= 1
         else:
             total_pair += max_pair
-
+    
     if total_pair > 1000000000:
-        return -1  
+        return -1
     
     return total_pair
