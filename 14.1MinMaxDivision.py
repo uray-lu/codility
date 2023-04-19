71c0040366dafd3f526c6dea0fe8a57dad4ab9eb
YU RUEI LU <80952286+uray-lu@users.noreply.github.com>
2023-04-19 22:14:06 +0800

    Update 14.1MinMaxDivision.py

--- 14.1MinMaxDivision.py
+++ 14.1MinMaxDivision.py
@@ -12,32 +12,35 @@ def get_blocks(K, A, Mid):
             blocks +=1
             temp_sum = digit
     
+    if A:
+        blocks = blocks +1
+    else:
+        blocks
     #因為可能會有空串列
-    return max(K,blocks +1)
-
-
+    return max(K,blocks)
 
 def solution(K, M, A):
     
-    
     if K == 1:
         return sum(A)
-
- 
+    
     upper = sum(A)
     lower = max(A)
     result = -1
-    
-    # Binary search loop between upper and lower
+
     while upper >= lower:
 
         mid = (upper+lower)//2
-        
-        blocks = get_blocks(K, A, mid)
+
+        blocks = get_blocks(K,A,mid)
 
         if blocks > K:
             lower = mid +1
+        ### if blocks <= K means I might find a minimal cadicate
+        ### cuz = means fit K, and < meas I'vd use all the number can add no matter how many empty [] to fit K
+        ### check if there is any lower value by update upper
         else:
+            
             if result == -1:
                 result = mid
             else:
